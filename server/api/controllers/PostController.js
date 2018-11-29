13fd47eea42c917a6714b8af2f483b632e549bf7
moses.adebayo <moses.adebayo@venturegardengroup.com>
2018-11-29 12:49:05 +0100

    Pagination added to post search

--- server/api/controllers/PostController.js
+++ server/api/controllers/PostController.js
@@ -10,42 +10,51 @@ const logger = new Logger('RM');
 
 module.exports = {
     create: async function(req, res){
-        Post.create(req.body)
-            .then(post => {
-                logger.info(`Created post ${post.title}`);
-                return res.json(Utilities.processResponse(post));
-        }).catch(err => {
-            const { error } = Utilities.processError(err);
+        try{
+           const post = await Post.create(req.body).fetch();
+            logger.info(`Post created: ${post.title}`);
+            return res.json(Utilities.processResponse(post));
+        }catch (err){
+            const error = Utilities.processModelError(err);
             logger.error(`Error in creating post: ${error}`);
-            return res.status(400).json({error});
-        });
+            return res.badRequest({error});
+        }
     },
     getById: async function(req, res){
-        Post.findOne(req.params.id)
-            .then(post => {
-                if(post){
-                    logger.info(`Get post by id: ${post.title}`);
-                    return res.json(Utilities.processResponse(post));
-                }else{
-                    logger.info(`Get post by id not found: ${req.params.id}`);
-                    return res.status(400).json({error: 'Post not found'});
-                }
-        }).catch(err => {
+        try{
+            const post = await Post.findOne(req.params.id);
+            if(post){
+                logger.info(`Get post by id: ${post.title}`);
+                return res.json(Utilities.processResponse(post));
+            }else{
+                logger.info(`Get post by id not found: ${req.params.id}`);
+                return res.status(400).json({error: 'Post not found'});
+            }
+        }catch (err){
             const { error } = Utilities.processError(err);
             logger.error(`Error in getting post: ${error}`);
             return res.status(400).json({error});
-        });
+        }
     },
     search: async function(req, res){
-        Post.find(req.body)
-            .then(posts => {
-                logger.info(`Search posts`);
-                return res.json(posts);
-        }).catch(err => {
-            const { error } = Utilities.processError(err);
-            logger.error(`Error in searching post: ${error}`);
-            return res.status(400).json({error});
-        });
+        let { size, page, title, description, id } = req.body;
+        size = (size) ? size : 10;
+        page = (page) ? page : 1;
+        const offset = size * (page - 1);
+        try{
+            const posts = await Post.find(
+                {
+                    title,
+                    id,
+                    description
+                })
+                .skip(offset)
+                .limit(size);
+            return res.json(Utilities.processResponse(posts));
+        } catch (err){
+            const error = Utilities.processModelError(err);
+            return res.badRequest({error});
+        }
     }
 
 };
