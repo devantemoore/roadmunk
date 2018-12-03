bdf72ec6f049ffb5855c51965a8323d4b691bd83
moses.adebayo <moses.adebayo@venturegardengroup.com>
2018-12-03 11:42:08 +0100

    React redux setup on project

---
+++ client/src/_constants/app.constants.js
@@ -0,0 +1,16 @@
+
+export const appConstants = {
+
+    NOTIFY_SUCCESS: 'success',
+    NOTIFY_ERROR: 'error',
+    NOTIFY_INFO: 'info',
+    NOTIFY_INVALID: 'in',
+    NOTIFY_WARNING: 'warning',
+    NOTIFY_DANGER: 'danger',
+
+    SUCCESS_RESPONSE: 1,
+    ERROR_RESPONSE: 2,
+
+    SIGN_IN_SUCCESS: 'POST_SIGN_IN_SUCCESS',
+    SIGN_OUT: 'SIGN_OUT',
+};
